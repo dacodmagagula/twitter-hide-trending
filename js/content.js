1eaa2fc3f4e46113e09190905aea291b2619bfed
Dacod Magagula <clerk.dacod@gmail.com>
2022-05-18 17:19:57 +0100

    removing 'Who to follow'

--- js/content.js
+++ js/content.js
@@ -1,7 +1,7 @@
 $(function() {
 
 $("head").append("<style>\
-				[aria-label='Timeline: Trending now'] {\
+				[aria-label='Timeline: Trending now'],[aria-label='Who to follow'] {\
   				display:none !important;\
 				}</style>");
 
